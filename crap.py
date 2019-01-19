d48a307bcb27d5e25f581fb0731e011ba03accf4
SuperDorage <SuperDorage@gmail.com>
2019-01-19 21:31:09 +0900

    TurotialsPoint

--- crap.py
+++ crap.py
@@ -71,8 +71,24 @@ class Vector:
     def __add__(self, other):
         return Vector(self.a + other.a, self.b + other.b)
 
-v1 = Vector(2,10)
-v2 = Vector(5,-2)
+#v1 = Vector(2,10)
+#v2 = Vector(5,-2)
+#print(v1 + v2)
 
-print(v1 + v2)
+
+class JustCounter:
+    __secretCount = 0
+
+    def count(self):
+        self.__secretCount += 1
+        print(self.__secretCount)
+
+
+counter = JustCounter()
+counter.count()
+counter.count()
+#print(counter.__secretCount)
+
+print(counter._JustCounter__secretCount)
+
 
